cd34cdddb4966a12a8074da37dcf3f110dd9c1eb
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2022-06-05 17:49:45 +0200

    Time: 0 ms (100.00%), Space: 40.5 MB (62.21%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        if(1 < n)
+            dp[1] = 2;
+        for(int i = 2; i < n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n-1];
+    }
+}
