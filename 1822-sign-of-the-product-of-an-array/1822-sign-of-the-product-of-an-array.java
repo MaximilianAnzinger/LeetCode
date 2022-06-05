23a3a2b0c1db5144ee4fb816883c11c18f8900c4
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2022-06-05 23:17:47 +0200

    Time: 0 ms (100.00%), Space: 44.1 MB (42.78%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int sign = 1;
+        for(int i : nums) {
+            if(i == 0)
+                return 0;
+            else if(i < 0)
+                sign = -sign;
+        }
+        return sign;
+    }
+}
