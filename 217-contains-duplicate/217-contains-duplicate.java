bd88f48098d591fc15836f673058c9102c98f5d2
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2022-06-12 13:32:16 +0200

    Time: 11 ms (60.01%), Space: 74 MB (5.15%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+		for(int i : nums)
+		    if(!set.add(i))
+		        return true; 
+		return false;
+    }
+}
