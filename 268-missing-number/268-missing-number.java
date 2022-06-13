07c03fa45a1f2a9a54a01d7289d6b61b0c34dfd9
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2022-06-13 15:21:47 +0200

    Time: 1 ms (68.06%), Space: 50.8 MB (64.52%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int missingNumber(int[] nums) {
         int r = 0;
         for(int i = 0; i < nums.length; i++)
-            r ^= nums[i] ^ i;
-        return r ^ nums.length;
+            r ^= nums[i]^i;
+        return r^nums.length;
     }
 }
