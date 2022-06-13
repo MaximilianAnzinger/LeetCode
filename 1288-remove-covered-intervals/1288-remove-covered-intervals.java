861fac4cf734805eab77cab94f1f393f71f75d34
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2022-06-13 15:28:53 +0200

    Time: 9 ms (32.33%), Space: 46.1 MB (60.30%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if(a[0] == b[0])
+                    return b[1] - a[1];
+                return a[0] - b[0];
+            }
+        });
+        int r = intervals.length;
+        int[] i = new int[] {-1, -1};
+        for(int[] interval : intervals)
+            if( i[0] <= interval[0] && interval[1] <= i[1])
+                r--;
+            else
+                i = interval;
+        return r;
+    }
+}
